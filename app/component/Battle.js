4c1ea053158b3ae3400dc354a7be26d17f1319e3
Darin <darinevanow@gmail.com>
2018-01-19 15:09:04 -0800

    Added routes to battle, popular, and 404, as well as built out the home component, and placeholder components for battle and 404

---
+++ app/component/Battle.js
@@ -0,0 +1,13 @@
+let React = require('react');
+
+class Battle extends React.Component {
+  render () {
+    return (
+      <div>
+        Battle
+      </div>
+    )
+  }
+}
+
+module.exports = Battle;
